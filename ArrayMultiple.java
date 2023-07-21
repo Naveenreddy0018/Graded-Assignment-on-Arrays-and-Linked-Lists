71f4283e12017caebaec5d0ec2826b3b6246e9de
Naveen Kumar Reddy Chinthakunta <89237138+Naveenreddy0018@users.noreply.github.com>
2023-07-21 10:25:23 +0530

    Create ArrayMultiple.java

---
+++ ArrayMultiple.java
@@ -0,0 +1,47 @@
+// Question 1: Cumulative Multiple of an Array with exception handling
+// Skill Mapping: Basic
+// Description: Consider an Array with N number of integers.
+// Task 1- WAP in Java to calculate cumulative multiple at each index.
+// Task 2 – Push the result into the same Array and Print.
+// Task 3 - Handle the exceptions in program
+// Sample input: arrNum = [5, 3, 4, 2, 0, 8]
+// Output:- arrNum = [5, 15, 60, 120, 0, 0]
+// Explanation:- arrNum[0] -> C.M. = 5
+// arrNum[1] -> C.M. = 5 x 3
+// arrNum[2] -> C.M. = 5 x 3 x 4
+// arrNum[3] -> C.M. = 5 x 3 x 4 x 2
+// arrNum[4] -> C.M. = 5 x 3 x 4 x 2 x 0
+// arrNum[5] -> C.M. = 5 x 3 x 4 x 2 x 0 x 8
+
+import java.util.Scanner;
+
+class ArrayMultiple {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arrNum[] = new int[n];
+        for(int i = 0; i < n; i++) {
+            arrNum[i] = sc.nextInt();
+        }
+
+        try {
+            int k = 0;
+            for(int i = 1; i < n; i++) {
+                arrNum[i] = arrNum[k] * arrNum[i];
+                k++;
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        for(int i = 0; i < n; i++) {
+            System.out.print(arrNum[i] + " ");
+        }
+
+        
+        
+        
+
+    }
+}
